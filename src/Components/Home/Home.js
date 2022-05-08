571ee91b11f6c7dac41ab88c0611b191b87b4559
Sajib Hossain <sajib.sj359@gmail.com>
2022-05-08 22:03:47 +0600

    added extra field in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,11 +5,26 @@ const Home = () => {
   return (
     <div> 
       <img src="https://waltonbd.com/image/catalog/refrigerator-and-freezer/non-frost.jpg" alt="" />
+      <div>
+      <h1 className="text-4xl md:text-6xl underline font-bold font-serif mt-5">Refrigerator <span className="text-red-700">Mart</span></h1>
+      <p className="text-2xl font-mono font-semibold text-blue-500 mt-3 mb-3">Choose Your Best Option From US.... Stay Safe Stay Happy</p>
+     <button className="bg-rose-300 p-2 mb-3">
+      <a className=" text-2xl text-blue-500" href="https://ministerbd.net/product-category/refrigerator-bangladesh/">Buy Now</a></button>
+      </div>
 
-    <h1 className="text-6xl underline font-bold font-serif mt-5">Refrigerator <span className="text-red-700">Mart</span></h1>
-    <p className="text-2xl font-mono font-semibold text-blue-500 mt-3 mb-3">Choose Your Best Option From US.... Stay Safe Stay Happy</p>
-    <button className="bg-rose-300 p-2 mb-3">
-    <a className=" text-2xl text-blue-500" href="https://ministerbd.net/product-category/refrigerator-bangladesh/">Buy Now</a></button>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mt-12  rounded-2xl p-5 bg-slate-100">
+      <div>
+        <img className="rounded-md" src="https://ministerbd.net/wp-content/uploads/2021/05/22-1024x576.jpg" alt="" />
+      </div>
+      <div className=" bg-white shadow-md justify-around p-5 rounded-3xl ">
+
+        <h1 className="md:mt-24 text-3xl font-bold text-red-700">WHO WE ARE</h1>
+      
+        <p className="text-2xl text-blue-500 mt-5">Minister Hi-Tech Park Limited is the fastest growing Electronics Industry in our country. Mr. M.A. Razzak Khan Raj, founder, MyOne Electronics Industries Limited, has established it on 01 June 2002. Later on he has also established Minister Hi-Tech Park Limited on 01 June 2013. More about Minister please visit: Company Profile</p>
+    
+        
+      </div>
+    </div>
       
     </div>
   );
